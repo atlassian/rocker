016ad61b8e5f440e75a220e16ab467a4c89fc038
Antoine Busch <abusch@atlassian.com>
2019-09-17 13:19:05 +1000

    Make docker info view a bit prettier

--- src/views/docker_info.rs
+++ src/views/docker_info.rs
@@ -4,6 +4,7 @@ use shiplift::rep::Info;
 use termion::event::Key;
 use tui::{
     layout::Rect,
+    style::{Color, Modifier, Style},
     widgets::{Block, Borders, Paragraph, Text, Widget},
     Frame,
 };
@@ -33,12 +34,49 @@ impl View for DockerInfo {
     }
 
     fn draw(&self, t: &mut Frame<Backend>, rect: Rect) {
-        let display_string = if let Some(ref info) = self.info {
-            format!("{:#?}", info)
+        let text = if let Some(ref info) = self.info {
+            vec![
+                Text::styled(
+                    "Host details\n",
+                    Style::default().modifier(Modifier::BOLD).fg(Color::Blue),
+                ),
+                Text::styled(format!("Hostname:       {}\n", info.name), Style::default()),
+                Text::styled(
+                    format!("OS Information: {}\n", info.operating_system),
+                    Style::default(),
+                ),
+                Text::styled(
+                    format!("Kernel Version: {}\n", info.kernel_version),
+                    Style::default(),
+                ),
+                Text::styled(
+                    format!("Total CPU:      {}\n", info.n_cpu),
+                    Style::default(),
+                ),
+                Text::styled(
+                    format!("Total Memory:   {}\n", info.mem_total),
+                    Style::default(),
+                ),
+                Text::raw("\n"),
+                Text::styled(
+                    "Engine details\n",
+                    Style::default().modifier(Modifier::BOLD).fg(Color::Blue),
+                ),
+                Text::styled(
+                    format!("Root Directory:       {}\n", info.docker_root_dir),
+                    Style::default(),
+                ),
+                Text::styled(
+                    format!("Storage Driver:       {}\n", info.driver),
+                    Style::default(),
+                ),
+            ]
         } else {
-            "Could not retrieve information from the Docker daemon.".to_string()
+            vec![Text::styled(
+                "Could not retrieve information from the Docker daemon.".to_string(),
+                Style::default(),
+            )]
         };
-        let text = vec![Text::raw(display_string)];
 
         Paragraph::new(text.iter())
             .block(Block::default().borders(Borders::ALL))
